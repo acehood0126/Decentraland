c7f758003e048cb7a9472ee9754e16c7cc52f1ef
robinhood8865 <robinhood8865@gmail.com>
2022-04-05 17:34:26 +0900

    Create an entity

--- src/game.ts
+++ src/game.ts
@@ -60,3 +60,18 @@ avocado.addComponent(
   })
 )
 engine.addEntity(avocado)
+
+// Create an entity
+const box = new Entity()
+
+// Create and add a `Transform` component to that entity
+box.addComponent(new Transform())
+
+// Set the fields in the component
+box.getComponent(Transform).position.set(3, 1, 3)
+
+// Create and apply a `BoxShape` component to give the entity a visible form
+box.addComponent(new BoxShape())
+
+// Add the entity to the engine
+engine.addEntity(box)
